f23a3d8f51dd87117b70923693c9c7b6339850e5
AiSuchodolski <szymon@aisuchodolski.pl>
2025-10-23 18:29:58 +0200

    Zakończony parser csv

--- Backend/parsers/csv_parser.py
+++ Backend/parsers/csv_parser.py
@@ -2,32 +2,52 @@ import os
 import glob
 import csv
 
-data = []
 
-
-def file_path_parser():
-    parsers = os.path.join("..", "upload", "*.csv")
-    csv_files = glob.glob(parsers)
+def csv_parser():
+    csv_path = os.path.join("..", "upload", "*.csv")
+    csv_files = glob.glob(csv_path)
     if not csv_files:
         print("No CSV files found in uploads folder")
         exit()
-    return csv_files
+    data_path = os.path.join("..", "data", "data.txt")
+    i = 1
+    for csv_file in csv_files:
+        with open(csv_file, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            with open(data_path, "a", encoding="utf-8") as data_file:
+                for row in reader:
+                    data_file.write(f'{i}. {row}\n')
+                    i += 1
+    print("CSV files parsed successfully and saved to data.txt")
 
-i = 0
-for file in file_path_parser():
-    with open(file, "r", encoding="utf-8") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            data.append(f'{i}. {row}')
-            i += 1
+
+csv_parser()
 
 
 
-def file_path_data():
-    data = os.path.join("..", "data", "data.txt")
-    data_file = open(data, "w", encoding="utf-8")
-    data_file.write(data)
-    data_file.close()
-    return data_file
+# def file_path_parser():
+#     parsers = os.path.join("..", "upload", "*.csv")
+#     csv_files = glob.glob(parsers)
+#     if not csv_files:
+#         print("No CSV files found in uploads folder")
+#         exit()
+#     return csv_files
 
-print(file_path_data())
+# i = 0
+# for file in file_path_parser():
+#     with open(file, "r", encoding="utf-8") as file:
+#         reader = csv.reader(file)
+#         for row in reader:
+#             data.append((f'{i}. {row}\n'))
+#             i += 1
+
+
+
+# def file_path_data():
+#     path_data = os.path.join("..", "data", "data.txt")
+#     data_file = open(path_data, "w", encoding="utf-8")
+#     data_file.write(''.join(data))
+#     data_file.close()
+#     print("Data saved to data.txt")
+
+# file_path_data()
